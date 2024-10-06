b5baaa43a69e9c244e805f2f9f1b262473547d9b
Abdurashid <khaidarovabdurashid@gmail.com>
2024-10-06 19:14:28 +0000

    d

---
+++ script.js
@@ -0,0 +1,24 @@
+let name, age;
+
+name = prompt("enter your name")
+console.log(`Your name is ${name}`);
+
+
+age = prompt("enter your age")
+console.log(`Your age is ${age}`);
+
+
+example1 = prompt(" 9 + 1 = ")
+console.log(`9 + 1 = 10, your answer ${example1}`);
+
+example2 = prompt(" 9 - 1 = ")
+console.log(`9 - 1 = 8, your answer ${example2}`);
+
+example3 = prompt(" 9 * 2 = ")
+console.log(`9 * 2 = 18, your answer ${example3}`);
+
+example3 = prompt(" 8 / 2 = ")
+console.log(`8 / 2 = 4, your answer ${example3}`);
+
+example3 = prompt(" 9 % 2 = ")
+console.log(`9 % 2 = 1, your answer ${example3}`);
